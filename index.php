14060952a8ebe414dd2b3024baa27fe890768fbd
ChrisSmith <chris.smith324@gmail.com>
2013-08-03 04:12:52 -0400

    fix script tags

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
   <head>
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	
-  	<link rel="stylesheet" href="/css/bootstrap.min"/>
+  	<link rel="stylesheet" href="/css/bootstrap.min"></link>
 
-  	<script type="text/javascript" src="/js/jquery.min.js" />
-  	<script type="text/javascript" src="/js/bootstrap.min.js" />
+  	<script type="text/javascript" src="/js/jquery.min.js"></script>
+  	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
   </head>
   <body>
   <div class="container">
