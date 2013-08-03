6d4699299499423fee1fbe39b6cc6750a7e54085
marcoazn89 <imnotmarco@live.com>
2013-08-03 15:00:17 -0400

    update

--- api.php
+++ api.php
@@ -1,10 +1,9 @@
 <?php
-//echo
 function connectDB(){
 	$db_host = "localhost"; 
 	$db_username = "root";  
 	$db_pass = "password";  
-	$db_name = "zematest"; 
+	$db_name = "kansasbbqueue"; 
 	$connection = mysqli_connect("$db_host","$db_username","$db_pass","$db_name");
 	if (!$connection) {
 	    die('Connect Error (' . mysqli_connect_errno() . ') '
@@ -22,11 +21,19 @@ function get_inserted_id($connection, $query) {
 }
 
 function getStamp($date, $timezone) {
+	date_default_timezone_set("America/New_York");
 	$date = new DateTime($dateStr, new DateTimeZone($timezone));
 	return strtotime($date);
 }
 
 function setDate($y, $m, $d, $h, $min, $sec) {
 	return $y."-".$m."-".$d." ".$h.":".$min.":".$sec;
+}
+
+function getCurrentTimestamp() {
+	date_default_timezone_set("America/New_York");
+	$date = date('Y-m-d H:i:s');
+	$time = strtotime('-4 hours', strtotime($date));
+	return $time;
 }
 ?>
--- database.php
+++ database.php
@@ -4,7 +4,7 @@ require_once('api.php');
 ?>
 
 <?php
-class database {
+class Database {
 	public function __construct(){
 	}
 	
@@ -12,24 +12,68 @@ class database {
 		$connection = connectDB();
 		$location = NULL;
 		
-		$query = "SELECT `address` FROM `profile` WHERE `phone` = '$phone'";
-
+		$query = "SELECT `phone` FROM `profile` WHERE `phone` = '$phone'";
 		$result = mysqli_query($connection, $query);
 		
 		if($result !== FALSE && mysqli_num_rows($result) > 0) {
-			//pull address
-			$row = mysqli_fetch_array($result, MYSQLI_BOTH);
-			$location = $row['address'];
-		}
-		elseif($result !== FALSE && mysqli_num_rows($result) == 0) {
-			//insert address
+			echo "1";
+		
+			$query = "SELECT `polling`.`polling` FROM `polling`,`profile` WHERE `polling`.`polling_id` = `profile`.`polling_id` AND `profile`.`phone` = '$phone'";
+	
+			$result = mysqli_query($connection, $query);
+			
+			if($result !== FALSE && mysqli_num_rows($result) > 0) {
+				//pull address
+				echo "2";
+				$row = mysqli_fetch_array($result, MYSQLI_BOTH);
+				$location = $row['polling'];
+			}
 		}
 		else {
-			//insert user
-			$query = "INSERT INTO `profile` VALUES (NULL, '$phone', NULL, NULL, NULL, NULL);";
+			$query = "INSERT INTO `profile` (`phone`) VALUES ('$phone');";
+			echo $query;
+			mysqli_query($connection, $query);
 		}
 		
+		mysqli_close($connection);
 		return $location;
+	}
+	
+	public function checkIn($phone, $numberOfPeople) {
+		$connection = connectDB();
+		$timestamp = getCurrentTimestamp();
+		$query = "SELECT `polling_id` FROM `profile` WHERE `phone` = '$phone'";
+		$result = mysqli_query($connection, $query);
+		$row = mysqli_fetch_array($result, MYSQLI_BOTH);
+		$polling_id = $row['polling_id'];
+		
+		$query = "INSERT INTO `time_present` (`polling_id`, `time`, `people_count`) VALUES ('$polling_id', FROM_UNIXTIME($timestamp), 10);";
+		$result = mysqli_query($connection, $query);
+		
+		mysqli_close($connection);
+		
+		return 0;
+	}
+	
+	public function reportBooth($phone, $numberOfBooth) {
+		$connection = connectDB();
+		//$timestamp = getCurrentTimestamp();
+		$query = "UPDATE `time_present` INNER JOIN `profile` ON `time_present`.`polling_id` = `profile`.`polling_id` SET  `time_present`.`booth_count` =  '$numberOfBooth' WHERE  `profile`.`phone` = '$phone';";
+		mysqli_query($connection, $query);
+		mysqli_close($connection);
+		
+		return 0;
+	}
+	
+	public function insertPollingAddress($phone, $address) {
+		$connection = connectDB();
+		
+		$query = "UPDATE `profile` SET  `address` =  '$address' WHERE  `phone` = '$phone';";
+		mysqli_query($connection, $query);
+		
+		mysqli_close($connection);
+		
+		return 0;
 	}
 }
 ?>
