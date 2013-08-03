c4e6718de288f61815b6095692d72b3930ed3d2e
marcoazn89 <imnotmarco@live.com>
2013-08-03 16:14:00 -0400

    x

--- getWait.php
+++ getWait.php
@@ -1,3 +1,8 @@
+<?php 
+/* Import API */
+require_once('api.php'); 
+?>
+
 <html lang="en">
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +23,35 @@
                 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 
 		<?php $addressstring = $_POST["street-num"]." ".$_POST["street"]." ".$_POST["city"]." ".$_POST["state"]; ?>
-
+		
+        <?php
+			$connect = connectDB();
+			
+			$google_location = "";//chris's function
+			
+			$past = translateTimeStamp(getHourlyTimestamp());
+			$present = $start = translateTimeStamp(getCurrentTimestamp());
+			
+			$query = "SELECT `people_count`,`booth_count` FROM `time_present` WHERE `time` BETWEEN '$past' AND '$present'";	
+			
+			$result = mysqli_query($connect,$query);
+			
+			$people_count = NULL;
+			$people_instance = NULL;
+			$booth_count = NULL;
+			$booth_instance = NULL;
+			$counter = 0;
+			while($row = mysqli_fetch_array($result, MYSQLI_BOTH)) { 
+				$counter++;
+				$people_count += $row['people_count'];
+				$booth_count += $row['booth_count'];
+			}
+			$people_instance = $counter;
+			$booth_instance = $counter;
+			
+			$waittime = getWaitTime($people_count, $people_instance, $booth_count, $booth_instance, $votingavg)
+		?>
+		
 		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
                 <script type="text/javascript" src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
--- test.php
+++ test.php
@@ -4,7 +4,8 @@ require_once('database.php');
 ?>
 
 <?php
-$dat = new Database();
-$dat->getLocationForUser('6546546546');
+//$dat = new Database();
+//$dat->getLocationForUser('6546546546');
+echo translateTimeStamp('1375537410');
 //$dat->insertAddress('6546546546','xxxxxx');
 ?>
