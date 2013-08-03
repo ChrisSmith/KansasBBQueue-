0aa4043a1d7ad538f429c1d4309130c215c96aab
ChrisSmith <chris.smith324@gmail.com>
2013-08-03 03:51:07 -0400

    add index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	
+  	<link rel="stylesheet" href="/css/bootstrap.min"/>
+
+  	<script type="text/javascript" src="/js/jquery.min.js" />
+  	<script type="text/javascript" src="/js/bootstrap.min.js" />
+  </head>
+
+  <div class="container">
+
+	<div class="row">
+	  <div class="col-12 col-lg-8">YOLO!</div>
+	</div>
+
+  </div>
+
+</html>
